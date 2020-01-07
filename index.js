18a0300900e248d3c8ea9c646e55586410088b89
Guilherme <marquesguilherme3@gmail.com>
2020-01-07 04:04:52 -0300

    Add comments

--- index.js
+++ index.js
@@ -9,22 +9,26 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies/:movieName', (req, res) => {
-    const { movieName } = req.params;
-    const movieFile = `${__dirname}/assets/${movieName}`;
+    const { movieName } = req.params; // Nome do arquivo
+    const movieFile = `${__dirname}/assets/${movieName}`; // Caminho para o arquivo
+
+    // fs.stat pega dados do arquivo de forma assíncrona
     fs.stat(movieFile, (err, stats) => {
         if (err) {
             console.log(err);
             return res.status(404).end('<h1>Movie Not found</h1>');
         }
         // Variáveis necessárias para montar o chunk header corretamente
-        const { range } = req.headers;
-        const { size } = stats;
+        const { range } = req.headers; // Byte atual que o video se encontra no momento da requisição
+        console.log(range)
+        const { size } = stats; // Tamanho do vídeo em bytes
+        // Recuperando o numero do byte a partir da string => "bytes={numero}-"
         const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
-        const end = size - 1;
-        const chunkSize = 65000;
+        const end = size - 1; // Último byte do vídeo
+        const chunkSize = size - 1; // Tamanho do arquivo para ser informado na requisição
         // Definindo headers de chunk
         res.set({
-            'Content-Range': `bytes ${start}-${end}/${size}`,
+            'Content-Range': `bytes ${start}-${end}/${size}`, // Imagino que seja o tamanho total que ainda falta do vídeo
             'Accept-Ranges': 'bytes',
             'Content-Length': chunkSize,
             'Content-Type': 'video/mp4'
@@ -33,9 +37,9 @@ app.get('/movies/:movieName', (req, res) => {
         res.status(206);
         // Utilizando ReadStream do Node.js
         // Ele vai ler um arquivo e enviá-lo em partes via stream.pipe()
-        const stream = fs.createReadStream(movieFile, { start, end });
-        stream.on('open', () => stream.pipe(res));
-        stream.on('error', (streamErr) => res.end(streamErr));
+        const stream = fs.createReadStream(movieFile, { start, end }); // Lê a parte necessária do arquivo (Bytes start -> end)
+        stream.on('open', () => stream.pipe(res)); // Escreve os dados na response quando o arquivo for aberto
+        stream.on('error', (streamErr) => res.end(streamErr)); // Retorna qualquer erro que tenha ocorrido
     });
 });
 
